e9c51434553526c12d53b28ece72c6f18bd28fb7
Vander Lima <vandercbl@gmail.com>
2020-04-07 23:14:37 -0300

    Desenvolvendo o código, baseado nos tests

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,73 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+	return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+	const { title, url, techs } = request.body
+
+	const repository = {
+		id: uuid(),
+		title,
+		url,
+		techs,
+		likes: 0
+	}
+
+	repositories.push(repository)
+
+	return response.json(repository)
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+	const { id } = request.params
+	const { title, url, techs } = request.body
+
+	if (!isUuid(id)) {
+		return response.status(400).json({ error: 'Id invalid format'})
+	}
+
+	const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+	if (repositoryIndex < 0) {
+		return response.status(401).json({ error: 'Id not found'})		
+	}
+
+	repositories[repositoryIndex].title = title
+	repositories[repositoryIndex].url = url
+	repositories[repositoryIndex].techs = techs
+
+	return response.json(repositories[repositoryIndex])
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+	const { id } = req.params
+
+	const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+	if (repositoryIndex < 0) {
+		return res.status(400).json({ error: 'Id not found'})
+	}
+
+	repositories.splice(repositoryIndex, 1)
+
+	return res.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+	const { id } = request.params
+
+	const repositoryIndex = repositories.findIndex(repository => repository.id === id)
+
+	if (repositoryIndex < 0) {
+		return response.status(400).json({ error: 'Id not found'})
+	}
+
+	repositories[repositoryIndex].likes += 1
+
+	return response.json(repositories[repositoryIndex])
 });
 
 module.exports = app;
