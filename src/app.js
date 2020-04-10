44cc5da67c06992fa4312d5085215bb32ee3394f
Vander Lima <vandercbl@gmail.com>
2020-04-10 04:12:51 -0300

    Adicionando log de tempo das requisições

--- src/app.js
+++ src/app.js
@@ -10,6 +10,20 @@ app.use(cors());
 
 const repositories = [];
 
+function loadRequests(request, response, next) {
+	const { method, url } = request
+
+	const logLabel = `[${method.toUpperCase()}] ${url}`
+
+	console.time(logLabel)
+	
+	next()
+
+	console.timeEnd(logLabel)
+}
+
+app.use(loadRequests)
+
 app.get("/repositories", (request, response) => {
 	return response.json(repositories)
 });
